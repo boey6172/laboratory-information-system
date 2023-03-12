a7a3f3fe57bc1774091087ba48fe2da0dc9d8978
Daniel Meynard Mabunga <boey6172@gmail.com>
2023-03-13 00:03:46 +0800

    feat: add roles for LIS

--- server/seeders/roles.js
+++ server/seeders/roles.js
@@ -1,20 +1,5 @@
 module.exports = {
     up: (queryInterface, Sequelize) => {
-      return queryInterface.bulkInsert('Genders', [{
-        id: '9c23bab0-5ecf-4568-a235-373c96632b67',
-        description: 'Male',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        id: 'd58b5a0d-f8c9-4a32-861a-d84547812ac9',
-        description: 'Female',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }
-    ]);
-    },
-    up: (queryInterface, Sequelize) => {
         return queryInterface.bulkInsert('Roles', [{
           id: 'd0eff7f7-2740-44ca-850f-836eb28093e6',
           description: 'User',
@@ -24,6 +9,24 @@ module.exports = {
         {
           id: 'c9cb1a54-3c62-4976-977f-5a1b5a8e494c',
           description: 'Admin',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          id: 'b6962235-1933-4eae-bda7-d4e92d84fe91',
+          description: 'Doctor',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          id: '4feb7d30-c245-4f21-8f03-11fd11d72c8a',
+          description: 'Examiner',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },        
+        {
+          id: '593fa9fa-68be-4da4-afc6-3eec60960ced',
+          description: 'Encoder',
           createdAt: new Date(),
           updatedAt: new Date()
         }
